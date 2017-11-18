e2d4db73687b5bf332c657c7f251b63dc1530a64
Robley Gori <robleyadrian@gmail.com>
2017-11-18 18:45:04 +0300

    [Chore] Add ports for the various environments

--- config.js
+++ config.js
@@ -2,17 +2,21 @@ module.exports = {
     'DEVELOPMENT': {
         'SECRET': 'sup3rs3cR3t_K__eY',
         'DATABASE': 'mongodb://localhost:27017/dev_dmsdb',
+        'PORT': 3001
     },
     'TESTING': {
         'SECRET': 'sup3rs3cR3t_K__eY',
         'DATABASE': 'mongodb://localhost:27017/test_dmsdb',
+        'PORT': 3002
     },
     'STAGING': {
         'SECRET': 'sup3rs3cR3t_K__eY',
         'DATABASE': 'mongodb://localhost:27017/staging_dmsdb',
+        'PORT': 3003
     },
     'PRODUCTION': {
         'SECRET': 'sup3rs3cR3t_K__eY',
         'DATABASE': 'mongodb://localhost:27017/prod_dmsdb',
+        'PORT': 3000
     }
 };
