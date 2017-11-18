2899edb01acbde263cdea8bf0a13ed2f870a115c
Robley Gori <robleyadrian@gmail.com>
2017-11-18 16:27:28 +0300

    [Chore] Refactor imports.

--- server/api/models/userModel.js
+++ server/api/models/userModel.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema,
+      bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
